5d9e8220b4bc7e9b2dece06db9ae810e6cc5a0cf
Dennis Wong <denniswong342@gmail.com>
2024-04-29 23:35:47 -0400

    Added drop down menu when user inputs games when they refuse to put steam ID.

--- gamesearch/src/main/java/UI/SearchWindow.java
+++ gamesearch/src/main/java/UI/SearchWindow.java
@@ -1,17 +1,25 @@
 package UI;
 
 import java.net.URL;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javax.naming.NameNotFoundException;
 import javax.swing.JOptionPane;
+
+import com.searchengine.Database;
 import com.searchengine.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.web.*;
+import org.controlsfx.control.textfield.TextFields;
+import org.w3c.dom.Text;
 
 public class SearchWindow extends WINDOW implements Initializable {
-    
+
     @FXML
     TextField query;
 
@@ -20,15 +28,21 @@ public class SearchWindow extends WINDOW implements Initializable {
     WebEngine engine;
 
     static Game search;
-    
-    public SearchWindow() 
+
+    public SearchWindow()
     {
         open("SearchWindow.fxml");
     }
 
     @FXML
-    public void query() 
+    public void query()
     {
+//        Database.init();
+//        List<String> gameNames = new ArrayList<>();
+//        for (Game game : Database.getAllGames()) {
+//            gameNames.add(game.getName());
+//        }
+//        TextFields.bindAutoCompletion(query, gameNames);
         try {
             engine.load(SearchApp.getSearchInstance().getUserData().addGame(query.getText()));
         } catch (NameNotFoundException nameNotFoundException) {
@@ -36,8 +50,8 @@ public class SearchWindow extends WINDOW implements Initializable {
         }
     }
 
-    @FXML 
-    public void search() 
+    @FXML
+    public void search()
     {
         SearchApp.display(GameWindow.presentGame(SearchApp.getSearchInstance().nextGame(), (long)1));
     }
@@ -45,5 +59,11 @@ public class SearchWindow extends WINDOW implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         engine = view.getEngine();
+        Database.init();
+        List<String> gameNames = new ArrayList<>();
+        for (Game game : Database.getAllGames()) {
+            gameNames.add(game.getName());
+        }
+        TextFields.bindAutoCompletion(query, gameNames);
     }
 }
